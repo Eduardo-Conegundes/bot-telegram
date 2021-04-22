2fb895e4da2bd5fc1e0f5015dc7f8d55a0edb676
Luiz Eduardo <duduconegundes@gmail.com>
2021-04-22 17:10:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const TelegramBot = require( `node-telegram-bot-api` );
+const TelegramBot = require(`node-telegram-bot-api` );
 
-const TOKEN = `831684431:AAHrJaV-JKHH4sxfii8EJK357QDJ7bRFP7g`
+const TOKEN = `831684431:AAHrJaV-JKHH4sxfii8EJK357QDJ7bRFP7g`;
 const bot = new TelegramBot( TOKEN, { polling: true } );
 
 bot.on(`message`, (msg) => {
-
+    data = msg;
     console.log(msg);
-    const Photo = `AgACAgEAAxkBAAIF92CBqwwYHzAw_zPjWuawCjWNoYwqAAJ2qTEbVSURRJmNN_HFGzFO1v_JShcAAwEAAwIAA20AA3JYAgABHwQ`
-    bot.sendPhoto(msg.chat.id, Photo);
-    //console.log(msg);
-    //console.log(Photo)
 
+    //caso seja uma foto
+    if(msg.photo){
+        const Photo = msg.photo[0].file_id;
+        bot.sendPhoto(msg.chat.id, Photo);
+    
+        if(msg.caption){
+            const legenda = msg.caption;
+            bot.sendMessage(msg.chat.id, legenda);
+        }
+    }
+
+    //caso seja mensagem de texto
+    if(data.text){ 
+        bot.sendMessage(msg.chat.id, msg.text);
+    }
+
+});
+
+bot.getChat(1111169549).then(function(chat){
+    console.log(chat.id);
+});
+
+//não é membro
+bot.getChat(-1001244772335).then(function(chat){
+    console.log(chat.id);
 });
 
 
